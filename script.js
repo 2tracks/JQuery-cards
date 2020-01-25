91f02464a244c9ad18ca9f33fefeeef010270597
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-01-25 13:11:48 +0000

    Added solution

--- script.js
+++ script.js
@@ -18,7 +18,37 @@ $(document).ready(function() {
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
-	});
+    });
+    
+    /*challenge one*/
+    $("p").text("Change the content of all p text's in one go!");
 
+    /*Challenge two */
+    $("p").append("<span>Add a span element to every p tag on the page");
+    $("a").remove();
+    $("div.card").empty();
+
+    /*Challenge tree */
+
+    // Creates a new script element
+    var script = document.createElement('script');
+    script.src = "//code.jquery.com/jquery-latest.min.js";
+    document.head.appendChild(script);
+
+    $("p").css("font-family");
+
+    $("h2").css("font-family");
+
+    $("p").text("Aaron");
+
+
+    // Creates a new script element
+    var script = document.createElement('script');
+    script.src = "//code.jquery.com/jquery-latest.min.js";
+    document.head.appendChild(script);
+
+    $("p").css("background-color", "green");
+
+    $("p").css("font-family", "Tahoma");
 
 }); 
